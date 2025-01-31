70c627df9edb9e86ff2b86b2baf1850dfcdbbe9d
Aditya Negi <119617732+Cyrus748@users.noreply.github.com>
2025-01-31 16:36:23 +0530

    Update useCompile.js

--- src/hooks/useCompile.js
+++ src/hooks/useCompile.js
@@ -10,39 +10,47 @@ const useCompile = (
 ) => {
   const [processing, setProcessing] = useState(false);
 
+  // Function to handle compilation
   const handleCompile = async () => {
     setProcessing(true);
 
     const formData = {
-      clientId: import.meta.env.VITE_RAPID_CLIENT_ID,
-      clientSecret: import.meta.env.VITE_RAPID_CLIENT_SECRET,
+      clientId: import.meta.env.VITE_CLIENT_ID,
+      clientSecret: import.meta.env.VITE_CLIENT_SECRET,
       script: code,
-      language: language.id,
-      versionIndex: "0", // Adjust based on the language version needed
+      language: language.name,
+      versionIndex: "0", // You can set this to the appropriate version index
     };
 
     try {
+      // Making a POST request to compile code
       const response = await axios.post(
         import.meta.env.VITE_RAPID_URL,
         formData,
         {
           headers: {
             "Content-Type": "application/json",
+            "X-RapidAPI-Host": import.meta.env.VITE_RAPID_HOST,
+            "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
           },
         }
       );
 
-      const output = response.data;
-      setOutputDetails(output);
+      setProcessing(false);
+      setOutputDetails(response.data); // Set the final output details
       showSuccessToast("Compiled Successfully!");
     } catch (error) {
       const status = error.response?.status;
-      if (status === 429) {
+      console.log("status", status);
+
+      if (status === 401) {
+        showErrorToast("Unauthorized access. Please check your credentials.");
+      } else if (status === 429) {
         showErrorToast("Quota of 100 requests exceeded for the Day!", 10000);
       } else {
         showErrorToast("Something went wrong. Try again!");
       }
-    } finally {
+
       setProcessing(false);
     }
   };
